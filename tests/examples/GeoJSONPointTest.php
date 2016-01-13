dfb1c75acda6a05a498251dec006a1cba3563a4b
luca@igor <luca@igor>
2016-01-13 22:38:54 +0100

    fix test

--- tests/examples/GeoJSONPointTest.php
+++ tests/examples/GeoJSONPointTest.php
@@ -13,7 +13,7 @@ class GeoJSONPointTest extends \PHPUnit_Framework_TestCase
     {
         $sut = new Point('ExamplePoint', [123, 456]);
         $formatter = new JsonFormatter();
-        $obtained = json_decode($formatter->buildContent($sut), false);
+        $obtained = json_decode($formatter->buildContent($sut));
 
 
         $this->assertEquals(123, $obtained->ExamplePoint->coordinates[0]);
@@ -21,8 +21,10 @@ class GeoJSONPointTest extends \PHPUnit_Framework_TestCase
         $object = new GeoJSONObject('ExampleObject');
         $object->setElement($sut, 1);
         $json = $formatter->buildContent($object);
+        $obtained = json_decode($json);
 
-        $this->assertEquals('{ExamplePoint:{"type":"Point","coordinates":[123,456]}}', $json);
+        $this->assertEquals('{"ExampleObject":{"ExamplePoint":{"type":"Point","coordinates":[123,456]}}}', $json);
+        $this->assertEquals(123, $obtained->ExampleObject->ExamplePoint->coordinates[0]);
 
     }
 
