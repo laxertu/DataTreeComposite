ec879a81840fbeb99d8f3e56badcf248f649a920
luca@igor <luca@igor>
2016-01-15 20:19:23 +0100

    PSR-2

--- tests/formatters/XMLFormatterTest.php
+++ tests/formatters/XMLFormatterTest.php
@@ -3,8 +3,7 @@ namespace MessageComposite\tests\formatters;
 
 use MessageComposite\Formatter\XMLFormatter;
 use MessageComposite\GenericMessage;
-use \MessageComposite\MessageElement;
-use \MessageComposite\MessageListOfValues;
+use MessageComposite\MessageElement;
 
 class XMLFormatterTest extends \PHPUnit_Framework_TestCase
 {
@@ -30,32 +29,6 @@ class XMLFormatterTest extends \PHPUnit_Framework_TestCase
 
 
     public function testListOfElements()
-    {
-        $list = new MessageElement('val', [1]);
-        $sut = new XMLFormatter();
-
-        $xml = $sut->buildContent($list);
-        $this->assertEquals('<val>1</val>', $xml);
-
-        $list = new MessageElement('val', [1, 2]);
-        $xml = $sut->buildContent($list);
-        $this->assertEquals('<val>1</val><val>2</val>', $xml);
-
-        $message = new GenericMessage('ListOfVals');
-        $message->setElement($list, 0);
-
-        $xml = $sut->buildContent($message);
-        $this->assertEquals('<ListOfVals><val>1</val><val>2</val></ListOfVals>', $xml);
-
-        $param = new MessageElement('param', 'param1');
-        $message->setElement($param, 1);
-        $xml = $sut->buildContent($message);
-        $this->assertEquals('<ListOfVals><val>1</val><val>2</val><param>param1</param></ListOfVals>', $xml);
-    }
-
-
-
-    public function ztestListOfElements()
     {
         $list = new MessageElement('val', [1]);
         $sut = new XMLFormatter();
