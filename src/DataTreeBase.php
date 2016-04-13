1ddbabd8207e7147dae7eb060903d086c326f8de
luca@igor <luca@igor>
2016-04-13 21:38:13 +0200

    minor change

--- src/DataTreeBase.php
+++ src/DataTreeBase.php
@@ -9,8 +9,8 @@ use laxertu\DataTree\Processor\ProcessableInterface;
  * Base class for different kinds of tree
  *
  * @see DataTree
- * Use this class for defining trees that have a named set of children. Children can be one of the following, or another
- * DataTree
+ * Use this class for defining trees that have a named set of children.
+ * Children can be one of the following, or another DataTree
  *
  * @see DataTreeList
  * if you want to build a list of trees
